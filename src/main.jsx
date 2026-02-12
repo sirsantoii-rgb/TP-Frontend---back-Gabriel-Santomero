962ae65abde558bb647bf197e4cdb7ee7e00e8b8
GabS.Dev <el_santofr@hotmail.com>
2026-02-12 16:51:04 +0100

    25

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
