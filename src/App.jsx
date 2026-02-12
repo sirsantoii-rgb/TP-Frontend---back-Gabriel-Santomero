12e80302cc08c4a8e7cfddaf3defbb3e514d0cb9
GabS.Dev <el_santofr@hotmail.com>
2026-02-12 16:49:22 +0100

    23

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router'
 import LoginScreen from './Screens/LoginScreen/LoginScreen'
 import RegisterScreen from './Screens/RegisterScreen/RegisterScreen'
 import ForgotPasswordScreen from './Screens/LoginScreen/ForgotPasswordScreen' // Nueva
