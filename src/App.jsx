1f3d75c2da767fd3bd0d0648d73f81af8f5d0ed6
GabS.Dev <el_santofr@hotmail.com>
2026-02-12 14:18:17 +0100

    11

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import LoginScreen from './Screens/LoginScreen/LoginScreen'
 import RegisterScreen from './Screens/RegisterScreen/RegisterScreen'
 import AuthContextProvider from './Context/AuthContext'
 import AuthMiddleware from './Middlewares/AuthMiddleware'
+import WorkspaceContextProvider from './Context/WorkspaceContext'
+import HomeScreen from './Screens/HomeScreen/HomeScreen'
+import CreateWorkspaceScreen from './Screens/CreateWorkspaceScreen/CreateWorkspaceScreen'
 
 function App() {
 
@@ -12,10 +15,15 @@ function App() {
     <AuthContextProvider>
       <Routes>
         <Route path='/' element={<LoginScreen />} />
-        <Route  path='/register' element={<RegisterScreen />} />
-        <Route  path='/login' element={<LoginScreen />} />
+        <Route path='/register' element={<RegisterScreen />} />
+        <Route path='/login' element={<LoginScreen />} />
         <Route element={<AuthMiddleware />}>
-          <Route path='/home' element={<h1>Home</h1>} />
+          <Route path='/home' element={
+            <WorkspaceContextProvider>
+              <HomeScreen />
+            </WorkspaceContextProvider>
+          } />
+          <Route path='/create-workspace' element={<CreateWorkspaceScreen />} />
         </Route>
       </Routes>
     </AuthContextProvider>
--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -5,19 +5,28 @@ export const AuthContext = createContext()
 
 export const AUTH_TOKEN_KEY = 'auth_token'
 
+function decodeAuthToken (auth_token){
+    return jwtDecode(auth_token)
+}
+
 function AuthContextProvider ({children}){
-    const [isLogged, setIsLogged] = useState(false)
-    const [session, setSession] = useState(null)
+    const auth_token = localStorage.getItem(AUTH_TOKEN_KEY)
+    const [isLogged, setIsLogged] = useState(Boolean(auth_token))
+    const [session, setSession] = useState(auth_token ? decodeAuthToken(auth_token) : null)
 
     useEffect(
         () => {
-           
-            const auth_token = localStorage.getItem(AUTH_TOKEN_KEY)
+            /* 
+            Normalmente los backends suelen tener un endpoint 
+            GET /api/auth/validate-token (Authorization: Bearer auth_token) Te responde si el token es valido o no
+            Este useEffect seria valido si en el futuro tenemos el GET /api/auth/validate-token
+            */
+            /* const auth_token = localStorage.getItem(AUTH_TOKEN_KEY)
             if(auth_token){
                 setIsLogged(true)
                 const session_decoded = jwtDecode(auth_token)
                 setSession(session_decoded)
-            }
+            } */
         }, 
         []
     )
