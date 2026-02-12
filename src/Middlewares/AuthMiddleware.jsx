e4e8cd8fd19b5fb085f33633a6e47506a431dcb4
GabS.Dev <el_santofr@hotmail.com>
2026-02-12 16:20:32 +0100

    18

--- src/Middlewares/AuthMiddleware.jsx
+++ src/Middlewares/AuthMiddleware.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { AuthContext } from "../Context/AuthContext"
-import { Navigate, Outlet } from "react-router"
+import { Navigate, Outlet } from "react-router-dom"
 
 function AuthMiddleware (){
     const {isLogged} = useContext(AuthContext)
